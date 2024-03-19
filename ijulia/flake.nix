b1aa6758b8dcac22711c53a5ef727c1b988e1271
Rellikeht <michaelhemperek@gmail.com>
2024-03-19 20:02:07 +0100

    added something

---
+++ ijulia/flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flakeUtils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flakeUtils,
+  }:
+    flakeUtils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        julia = pkgs.julia-bin;
+        buildInputs = with pkgs; ([
+            julia
+          ]
+          ++ (with python311Packages; [
+            notebook
+            jupyter-console
+            jupyter-core
+            jupyterlab
+          ]));
+        def = with pkgs;
+          mkShell {
+            inherit buildInputs;
+          };
+      in {
+        devShells = {
+          default = def;
+          shellHook = ''
+            echo shell hook yay
+          '';
+        };
+
+        packages.default = pkgs.writeScriptBin "ijulia" ''
+          ${julia}/bin/julia -e "
+          using Pkg
+          Pkg.activate(\".\")
+          using IJulia
+          IJulia.jupyterlab(dir=pwd())
+          "
+        '';
+
+        # An app that uses the `runme` package
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/ijulia";
+        };
+      }
+    );
+}
