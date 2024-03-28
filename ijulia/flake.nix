3fea3e77d41cfbfe002dd56c060ffff33ec6328c
Rellikeht <michaelhemperek@gmail.com>
2024-03-28 11:27:40 +0100

    shit

--- ijulia/flake.nix
+++ ijulia/flake.nix
@@ -2,34 +2,50 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flakeUtils.url = "github:numtide/flake-utils";
+    nix-gl.url = "github:nix-community/nixGL";
   };
 
   outputs = {
     self,
     nixpkgs,
     flakeUtils,
+    nix-gl,
   }:
     flakeUtils.lib.eachDefaultSystem (
       system: let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [nix-gl.overlay];
+        };
         common = import ../jupyter.nix {inherit pkgs;};
         utils = import ../utils.nix {inherit pkgs;};
 
         julia = pkgs.julia-bin;
         packages =
           common.shell-packages
-          ++ [julia]
+          # ++ (with pkgs; [nixgl.auto.nixGLDefault])
+          ++ []
           ++ (common.python-packages pkgs.python311Packages)
-          ++ (with self.packages.${system}; [run]);
+          ++ (with self.packages.${system}; [
+            ijulia-run
+            julia-run
+          ]);
       in {
         devShells = {
           default = utils.defaultShell packages;
         };
 
-        packages =
-          rec {
-            run = pkgs.writeScriptBin "ijulia" ''
-              ${julia}/bin/julia -e '
+        packages = let
+          # nixgl-run = "${pkgs.nixgl.nixGL}/bin/nixGL";
+        in
+          rec
+          {
+            # ${nixgl-run} ${julia}/bin/julia $@
+            julia-run = pkgs.writeScriptBin "julia" ''
+              ${julia}/bin/julia $@
+            '';
+            ijulia-run = pkgs.writeScriptBin "ijulia" ''
+              ${julia-run}/bin/julia -e '
               using Pkg
               Pkg.activate(".")
               if !haskey(Pkg.project().dependencies, "IJulia")
@@ -39,14 +55,14 @@
               IJulia.jupyterlab(dir=pwd())
               '
             '';
-            default = run;
+            default = ijulia-run;
           }
           // common.out-packages;
 
         apps = rec {
           run = {
             type = "app";
-            program = "${self.packages.${system}.run}/bin/ijulia";
+            program = "${self.packages.${system}.ijulia-run}/bin/ijulia";
           };
           default = run;
         };
