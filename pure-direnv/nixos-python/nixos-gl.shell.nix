60e99cd8c85a41fdd62ce546067d4a1b8da734bc
Rellikeht <michaelhemperek@gmail.com>
2024-10-26 11:37:36 +0200

    deps update + libs compatibility fix

--- pure-direnv/nixos-python/nixos-gl.shell.nix
+++ pure-direnv/nixos-python/nixos-gl.shell.nix
@@ -3,13 +3,15 @@
     zlib
     pkgs.stdenv.cc.cc.lib
     glib
+    libGL
   ];
   ld_path = "${pkgs.lib.makeLibraryPath buildInputs}";
+  gl_drv_path = "/run/opengl-driver/lib";
 in
   pkgs.mkShell {
     inherit buildInputs;
     phases = [];
     shellHook = ''
-      LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${ld_path}"
+      LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${gl_drv_path}:${ld_path}"
     '';
   }
